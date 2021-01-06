cf1c11128953322b5b7e4dc75b1dae9f66ef8cd8
Eliseu Sobral Santos <eliseusobralsantos28@gmail.com>
2021-01-05 22:50:23 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/aplicativo/de/entregas/AplicativoDeEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aplicativo.de.entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoDeEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoDeEntregasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/aplicativo/de/entregas/AplicativoDeEntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aplicativo.de.entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoDeEntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
