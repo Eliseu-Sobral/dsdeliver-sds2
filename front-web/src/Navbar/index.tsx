3a86262dc5e2ed1975c9f777c3e9fddf36b776b3
Eliseu Sobral Santos <eliseusobralsantos28@gmail.com>
2021-01-10 15:17:13 -0300

    Add routes and StepsHeader

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import './style.css';
 import { ReactComponent as Logo } from './logo.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="main-navbar">
             <Logo />
-            <a className="link" href="">Ds Delivery</a>
+            <Link to = "/" className="link" >Ds Delivery</Link>
         </nav>
 
     )
---
+++ front-web/src/Order/index.tsx
@@ -0,0 +1,18 @@
+import SteapsHeader from './SteapsHeader';
+import './style.css';
+
+
+function Order() {
+    return (
+        
+        <div className = "orders-container">
+
+            <SteapsHeader />
+        </div>
+
+    )
+    
+
+}
+
+export default Order;
